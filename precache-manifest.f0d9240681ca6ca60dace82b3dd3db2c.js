51f9166c513f4e22ae7cd3f3d3d409afb434df65
Krystian Dziuk <dziukers@gmail.com>
2018-12-10 20:37:55 +0100

    Updates

---
+++ precache-manifest.f0d9240681ca6ca60dace82b3dd3db2c.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "b093b5a2594bce58bc3def5986377496",
+    "url": "/Portfolio/static/media/dragonball-min.b093b5a2.jpg"
+  },
+  {
+    "revision": "a90222ffeb8449c4c891",
+    "url": "/Portfolio/static/css/main.d0e0c342.chunk.css"
+  },
+  {
+    "revision": "c6a867028e4c1fb8e4233712eb246231",
+    "url": "/Portfolio/static/media/fontello.c6a86702.svg"
+  },
+  {
+    "revision": "7a599bdbf9ec584c9c75",
+    "url": "/Portfolio/static/js/1.7a599bdb.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "299e78c62c1e68cbb515395a2181e6bb",
+    "url": "/Portfolio/static/media/bricks-min.299e78c6.jpg"
+  },
+  {
+    "revision": "274b59cdfa5d900823d4a8721a40d55b",
+    "url": "/Portfolio/static/media/bricks-mobile.274b59cd.jpg"
+  },
+  {
+    "revision": "45fe539046f0122b31aa6688e635eae3",
+    "url": "/Portfolio/static/media/face-min.45fe5390.png"
+  },
+  {
+    "revision": "d5855ba79ad4e70b5a74105031c8014c",
+    "url": "/Portfolio/static/media/starwars-min.d5855ba7.jpg"
+  },
+  {
+    "revision": "fdd519790c7908d8ba03abf21ac2d361",
+    "url": "/Portfolio/static/media/portfolio-min.fdd51979.jpg"
+  },
+  {
+    "revision": "a6341c40f603c9317f5688639deb38df",
+    "url": "/Portfolio/static/media/krainaindyka-min.a6341c40.jpg"
+  },
+  {
+    "revision": "62241e63c539599b3573574128a65b51",
+    "url": "/Portfolio/static/media/solarsystem-min.62241e63.jpg"
+  },
+  {
+    "revision": "a90222ffeb8449c4c891",
+    "url": "/Portfolio/static/js/main.a90222ff.chunk.js"
+  },
+  {
+    "revision": "046020e24ca2403bdc57ef9041d5c3c7",
+    "url": "/Portfolio/static/media/bggenerator-min.046020e2.jpg"
+  },
+  {
+    "revision": "943e3f27f2e3435f487089db4c1eb730",
+    "url": "/Portfolio/static/media/imagerecognition.943e3f27.JPG"
+  },
+  {
+    "revision": "490d4fb505a97f64c4fae14a3224a3c6",
+    "url": "/Portfolio/static/media/tinyone.490d4fb5.JPG"
+  },
+  {
+    "revision": "5e33bfe4d9c84d8f97161fa3994abffc",
+    "url": "/Portfolio/static/media/codingchallenge11.5e33bfe4.JPG"
+  },
+  {
+    "revision": "75956afdbc75d7deecdc2274acbf93e1",
+    "url": "/Portfolio/static/media/CV-Krystian Dziuk.75956afd.pdf"
+  },
+  {
+    "revision": "f65c4e33b7972cdc648f19bdf7c27014",
+    "url": "/Portfolio/static/media/fog-horizontal.f65c4e33.png"
+  },
+  {
+    "revision": "f96a5d4c6c7e8e3df62270c908c8ffa4",
+    "url": "/Portfolio/static/media/fontello.f96a5d4c.eot"
+  },
+  {
+    "revision": "48363e9d231a57ec17b15ed3cec7a390",
+    "url": "/Portfolio/static/media/fontello.48363e9d.woff2"
+  },
+  {
+    "revision": "0539aaa2057c95941716501fb5f53e6d",
+    "url": "/Portfolio/static/media/fontello.0539aaa2.woff"
+  },
+  {
+    "revision": "48f9f81796fa5e25101ea6715133d64a",
+    "url": "/Portfolio/static/media/fontello.48f9f817.ttf"
+  },
+  {
+    "revision": "7a599bdbf9ec584c9c75",
+    "url": "/Portfolio/static/css/1.f4a37873.chunk.css"
+  },
+  {
+    "revision": "c665b518fb3247bba0ffe16bee7945ab",
+    "url": "/Portfolio/index.html"
+  }
+];
